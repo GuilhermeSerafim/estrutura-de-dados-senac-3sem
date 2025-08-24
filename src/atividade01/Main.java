4814052781e4555807df58b4a30a45f32e504378
GuilhermeSerafim <guilerstudies@gmail.com>
2025-08-24 15:49:39 -0300

    feat: incluir livro

--- src/atividade01/Main.java
+++ src/atividade01/Main.java
@@ -2,6 +2,8 @@ package atividade01;
 
 import java.util.Scanner;
 
+import atividade01.enums.Tema;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -29,6 +31,21 @@ public class Main {
         }
     }
 
+    // Ações
+    private static void incluirLivro(Scanner in, Biblioteca bib) {
+        System.out.println("\n--- Incluir Livro ---");
+        System.out.println("Título: ");
+        String titulo = in.nextLine().trim();
+
+        System.out.println("Autor: ");
+        String autor = in.nextLine().trim();
+
+        Tema tema = lerTema(in);
+
+        boolean ok = bib.adicionarLivro(new Livro(titulo, autor, tema));
+        System.out.println(ok ? "✔ Incluído com sucesso." : "✘ Falha ao incluir (dados inválidos ou acervo cheio).");
+    }
+
     private static Object removerLivro(Scanner in, Biblioteca bib) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'removerLivro'");
@@ -49,14 +66,48 @@ public class Main {
         throw new UnsupportedOperationException("Unimplemented method 'letInt'");
     }
 
-    private static void incluirLivro(Scanner in, Biblioteca bib) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'incluirLivro'");
-    }
-
     private static void mostrarMenu() {
 
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'mostrarMenu'");
+    }
+
+    // Utils
+    private static Tema lerTema(Scanner in) {
+        while (true) {
+            System.out.println("tema:");
+            imprimirTemasNumerados();
+            int n = lerInt(in, "Escolha (número): ");
+            Tema t = temaPorIndice(n);
+            if (t != null)
+                return t;
+            System.out.println("Opção de tema inválida.\n");
+        }
+    }
+
+    private static int lerInt(Scanner in, String prompt) {
+        while (true) {
+            try {
+                System.out.println(prompt);
+                String s = in.nextLine().trim();
+                return Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um número inteiro válido.");
+            }
+        }
+    }
+
+    private static Tema temaPorIndice(int n) {
+        Tema[] valores = Tema.values();
+        if (n >= 1 && n <= valores.length)
+            return valores[n - 1];
+        return null;
+    }
+
+    private static void imprimirTemasNumerados() {
+        Tema[] valores = Tema.values();
+        for (int i = 0; i < valores.length; i++) {
+            System.out.printf("%d) %s%n", i + 1, valores[i].getDescricao());
+        }
     }
 }
