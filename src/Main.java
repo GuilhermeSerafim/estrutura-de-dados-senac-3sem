2792e14de5a9aec43acdaa7a0f55cfde58133da7
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-05 20:52:35 -0300

    teste stack

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Pilha<Integer> pilha = new Pilha<Integer>();
 
-        for (int i = 1; i < 11; i++) {
-            pilha.adiciona(i);
-        }
+        pilha.adiciona(1);
+        pilha.adiciona(2);
+        pilha.adiciona(3);
 
         System.out.println("Dados da Pilha: " + pilha.toString());
         System.out.println("Quantidade de dados na Pilha: " + pilha.tamanho());
---
+++ src/Teste.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class Teste {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        System.out.println(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.size());
+        System.out.println(stack);
+        // Verifica topo
+        System.out.println(stack.peek());
+
+        // Método para desempilhar
+        System.out.println(stack.pop());
+
+        System.out.println(stack);
+    }
+}
