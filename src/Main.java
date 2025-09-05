c8247261a510423cac19b4f165ebecf8a284ea79
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-05 20:38:15 -0300

    Desempilhando - aplicando conceito de LIFO

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Pilha<Integer> pilha = new Pilha<Integer>();
 
-        System.out.println("Verifica se a Pilha está vazia: " + pilha.estaVazia());
-        System.out.println("Informação no Topo da Pilha: " + pilha.topo());
-
         for (int i = 1; i < 11; i++) {
             pilha.adiciona(i);
         }
-        
-        System.out.println("Pilha está vazia? " + pilha.estaVazia());
-        System.out.println("Informação no Topo da Pilha: " + pilha.topo());
-        
+
         System.out.println("Dados da Pilha: " + pilha.toString());
         System.out.println("Quantidade de dados na Pilha: " + pilha.tamanho());
 
+        pilha.desempilha();
+
+        System.out.println("Dados da Pilha: " + pilha.toString());
+        System.out.println("Quantidade de dados na Pilha: " + pilha.tamanho());
 
     }
 }
--- src/Pilha.java
+++ src/Pilha.java
@@ -20,5 +20,11 @@ public class Pilha<T> extends EstruturaEstatica<T> {
             return this.elementos[tamanho - 1];
     }
 
-
+    public T desempilha() {
+        if (this.estaVazia())
+            return null;
+        T elemento = this.elementos[tamanho - 1];
+        super.remove(tamanho - 1);
+        return elemento;
+    }
 }
