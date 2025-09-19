460ea7bc7bc8088e935069d604777fefaecde721
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-19 19:52:25 -0300

    Aula 5 - lista encadeada - adicionando elemento

---
+++ src/aula05/ListaEncadeada.java
@@ -0,0 +1,52 @@
+package aula05;
+
+public class ListaEncadeada<T> {
+    private NO inicio;
+    private NO fim;
+    private int tamanho;
+
+    public ListaEncadeada() {
+        tamanho = 0;
+    }
+
+    public NO getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(NO inicio) {
+        this.inicio = inicio;
+    }
+
+    public NO getFim() {
+        return fim;
+    }
+
+    public void setFim(NO fim) {
+        this.fim = fim;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada [inicio=" + inicio + ", fim=" + fim + ", tamanho=" + tamanho + "]";
+    }
+
+    public void adiciona(T elemento) {
+        NO<T> celula = new NO<T>(elemento);
+        if (this.inicio == null && this.fim == null) {
+            this.inicio = celula;
+            this.fim = celula;
+        } else {
+            this.fim.setProximo(celula);
+            this.fim = celula;
+        }
+        this.tamanho++;
+    }
+}
---
+++ src/aula05/NO.java
@@ -0,0 +1,37 @@
+package aula05;
+
+public class NO<T> {
+    private T elemento;
+    private NO proximo;
+    
+    public NO(T elemento) {
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+    
+    public NO(T elemento, NO proximo) {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+    
+    public T getElemento() {
+        return elemento;
+    }
+    
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+    
+    public NO getProximo() {
+        return proximo;
+    }
+    
+    public void setProximo(NO proximo) {
+        this.proximo = proximo;
+    }
+    
+    @Override
+    public String toString() {
+        return "NO [elemento=" + elemento + ", proximo=" + proximo + "]";
+    }
+}
