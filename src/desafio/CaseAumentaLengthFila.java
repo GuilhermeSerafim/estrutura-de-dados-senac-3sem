26850eefa6cfa2b12896e620bc9a4cd486257a8f
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-19 15:02:17 -0300

    Case enfileira e espia fila

--- src/desafio/CaseAumentaLengthFila.java
+++ src/desafio/CaseAumentaLengthFila.java
@@ -9,21 +9,21 @@ public class CaseAumentaLengthFila {
 
         System.out.println(fila.getCount());
         System.out.println(fila.getLength());
-        fila.adiciona(1);
-        fila.adiciona(2);
-        fila.adiciona(3);
-        fila.adiciona(4);
-        fila.adiciona(5);
-        fila.adiciona(6);
-        fila.adiciona(7);
-        fila.adiciona(8);
-        fila.adiciona(9);
-        fila.adiciona(10);
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
+        fila.enfileira(4);
+        fila.enfileira(5);
+        fila.enfileira(6);
+        fila.enfileira(7);
+        fila.enfileira(8);
+        fila.enfileira(9);
+        fila.enfileira(10);
         System.out.println(fila.getCount());
         System.out.println(fila.getLength());
 
-        System.out.println("Adicionando um a mais do que o range...");
-        fila.adiciona(11);
+        System.out.println("enfileirando um a mais do que o range...");
+        fila.enfileira(11);
         System.out.println("Tamanho da fila atualizado (tamanhoAtual * 2): " + fila.getLength());
         in.close();
     }
--- src/desafio/CaseRemoveFila.java
+++ src/desafio/CaseRemoveFila.java
@@ -3,9 +3,9 @@ package desafio;
 public class CaseRemoveFila {
     public static void main(String[] args) {
         Fila<Integer> fila = new Fila<Integer>(10);
-        fila.adiciona(1);
-        fila.adiciona(2);
-        fila.adiciona(3);
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
         System.out.println(fila);
         System.out.println("Quantidades preenchidas da fila: " + fila.getCount());
 
