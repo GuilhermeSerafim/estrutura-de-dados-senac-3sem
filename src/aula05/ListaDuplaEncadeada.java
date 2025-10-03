40e919271b6688fdd75f4e93826b5f42664136e3
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-03 16:28:23 -0300

    Lista duplamente encadeada

---
+++ src/aula05/ListaDuplaEncadeada.java
@@ -0,0 +1,103 @@
+package aula05;
+
+public class ListaDuplaEncadeada<T> {
+    private NO<T> inicio;
+    private NO<T> fim;
+    private int tamanho;
+
+    public NO<T> getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(NO<T> inicio) {
+        this.inicio = inicio;
+    }
+
+    public NO<T> getFim() {
+        return fim;
+    }
+
+    public void setFim(NO<T> fim) {
+        this.fim = fim;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada [inicio=" + inicio + ", fim=" + fim + ", tamanho=" + tamanho + "]";
+    }
+
+    public void adiciona(T elemento) {
+        NO<T> celula = new NO<T>(elemento);
+        if (this.inicio == null && this.fim == null) {
+            this.inicio = celula;
+            this.fim = celula;
+        } else {
+            celula.setAnterior(celula);
+            this.fim.setProximo(celula);
+            this.fim = celula;
+        }
+        this.tamanho++;
+    }
+
+    public NO<T> get(int posicao) {
+        if (posicao < 0 || posicao > tamanho) {
+            return null;
+        }
+        NO<T> atual = this.inicio;
+        for (int i = 0; i < posicao; i++) {
+            if (atual.getProximo() != null) {
+                atual = atual.getProximo();
+            }
+        }
+        return atual;
+    }
+
+    public int buscar(T elemento) {
+        NO<T> atual = this.inicio;
+        for (int i = 0; i < this.getTamanho(); i++) {
+            if (atual.getElemento().equals(elemento)) {
+                return i;
+            }
+            atual = atual.getProximo();
+        }
+        return -1;
+    }
+
+    public void remover(T elemento) {
+        NO<T> anterior = null;
+        NO<T> atual = this.inicio;
+
+        // Looping e verificação de identificação do elemento
+        for (int i = 0; i < getTamanho(); i++) {
+            if (atual.getElemento().equals(elemento)) {
+                if (this.tamanho == 1) {
+                    this.inicio = null;
+                    this.fim = null;
+                } else if (atual == inicio) {
+                    atual.getProximo().setAnterior(null);
+                    this.inicio = atual.getProximo();
+                    atual.setProximo(null);
+                } else if (atual == fim) {
+                    this.fim = anterior;
+                    anterior.setProximo(null);
+                } else {
+                    anterior.setProximo(atual.getProximo());
+                    atual.getProximo().setAnterior(anterior);
+                }
+                atual = null;
+                this.tamanho--;
+                break;
+            }
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+    }
+}
--- src/aula05/NO.java
+++ src/aula05/NO.java
@@ -3,35 +3,47 @@ package aula05;
 public class NO<T> {
     private T elemento;
     private NO<T> proximo;
-    
+    private NO<T> anterior;
+
+    public NO<T> getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NO<T> antes) {
+        this.anterior = antes;
+    }
+
     public NO(T elemento) {
         this.elemento = elemento;
+        this.anterior = null;
         this.proximo = null;
     }
-    
-    public NO(T elemento, NO<T> proximo) {
+
+    public NO(T elemento, NO<T> proximo, NO<T> anterior) {
         this.elemento = elemento;
         this.proximo = proximo;
+        this.anterior = anterior;
     }
-    
+
     public T getElemento() {
         return elemento;
     }
-    
+
     public void setElemento(T elemento) {
         this.elemento = elemento;
     }
-    
+
     public NO<T> getProximo() {
         return proximo;
     }
-    
+
     public void setProximo(NO<T> proximo) {
         this.proximo = proximo;
     }
-    
+
     @Override
     public String toString() {
-        return "NO [elemento=" + elemento + ", proximo=" + proximo + "]";
+        return "NO [elemento=" + elemento + ", proximo=" + proximo + ", anterior=" + anterior + "]";
     }
+
 }
