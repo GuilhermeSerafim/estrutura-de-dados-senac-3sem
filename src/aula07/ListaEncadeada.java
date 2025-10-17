a2e0b17b67f80465389582d100dd12cb1f5e8860
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-17 16:05:57 -0300

    rev linkedlist

--- src/aula07/ListaEncadeada.java
+++ src/aula07/ListaEncadeada.java
@@ -3,17 +3,22 @@ package aula07;
 public class ListaEncadeada<T> {
     private No inicio;
     private int tamanho = 0;
-    private No fim;
+    private No ultimo;
 
     public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
-        this.inicio = celula;
+        if (this.tamanho == 0) {
+            this.inicio = celula;
+        } else {
+            this.ultimo.setProximo(celula);
+        }
+        this.ultimo = celula;
         this.tamanho++;
     }
 
     @Override
     public String toString() {
-        return "ListaEncadeada [inicio=" + inicio + ", tamanho=" + tamanho + ", fim=" + fim + "]";
+        return "ListaEncadeada [inicio=" + inicio + ", tamanho=" + tamanho + ", ultimo=" + ultimo + "]";
     }
 
     public int getTamanho() {
--- src/aula07/ListaEncadeadaTeste.java
+++ src/aula07/ListaEncadeadaTeste.java
@@ -6,6 +6,8 @@ public class ListaEncadeadaTeste {
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
         System.out.println("Tamanho: " + lista.getTamanho());
         lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
         System.out.println("Tamanho: " + lista.getTamanho());
         System.out.println(lista);
     }
