2e25a2625395fa43785b0c2a38bdca2985cd93a6
GuilhermeSerafim <guilerstudies@gmail.com>
2025-11-17 15:48:27 -0300

    docs: entendendo melhor busca em largura

--- src/aula07/ExemploGrafo.java
+++ src/aula07/ExemploGrafo.java
@@ -3,11 +3,13 @@ package aula07;
 public class ExemploGrafo {
     public static void main(String[] args) {
         Grafo<String> grafo = new Grafo<String>();
+        // Agora imprimiu o Claudio pois foi o primeiro a ser adicionado
+        // Fazendo com que a busca em largura inicie por ele
+        grafo.adicionarVertice("Claudio");
         grafo.adicionarVertice("Paulo");
         grafo.adicionarVertice("Lorenzo");
         grafo.adicionarVertice("Cleuza");
         grafo.adicionarVertice("Kleber");
-        grafo.adicionarVertice("Claudio");
 
         grafo.adicionarAresta("Paulo", "Lorenzo");
         grafo.adicionarAresta("Paulo", "Cleuza");
