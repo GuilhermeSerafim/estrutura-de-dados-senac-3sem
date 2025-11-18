5e68b4b67da1e22d1085354668eb8641d2f563fa
GuilhermeSerafim <guilerstudies@gmail.com>
2025-11-18 10:23:33 -0300

    feat: implement insert method in Arvore class and rename No class fields for clarity

--- src/aula08/Arvore.java
+++ src/aula08/Arvore.java
@@ -2,4 +2,25 @@ package aula08;
 
 public class Arvore {
     No root = null;
+
+    public void insert(int info, No place) {
+        if(place == null) {
+            root = new No(info);
+            System.out.println(" " + info + " (raiz)");
+        } else if(info < place.info) {
+            if(place.left == null) {
+                place.left = new No(info);
+                System.out.println(" " + info);
+            } else {
+                insert(info, place.left);
+            }
+        } else if(info > place.info) {
+            if(place.right == null) {
+                place.right = new No(info);
+                System.out.println(" " + info);
+            } else {
+                insert(info, place.right);
+            }
+        }
+    }
 }
--- src/aula08/No.java
+++ src/aula08/No.java
@@ -2,14 +2,14 @@ package aula08;
 
 public class No {
     int info;
-    No direita;
-    No esquerda;
+    No right;
+    No left;
     // int level;
     // No pai; // Isso permite que não precisamos usar recursão para subir na árvore
 
     public No(int info) {
         this.info = info;
-        this.direita = null;
-        this.esquerda = null;
+        this.right = null;
+        this.left = null;
     }
 }
